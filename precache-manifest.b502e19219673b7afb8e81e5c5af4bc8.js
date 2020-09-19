fcf5d2870cdbabe7c6171b05ae20ed12d8cdf7b7
Zeel-log <zeelm2014@gmail.com>
2020-09-19 15:36:46 +0530

    Updates

---
+++ precache-manifest.b502e19219673b7afb8e81e5c5af4bc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eca8882087425339dd8de1c80245352",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "9e4863a067f4b6df1778",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "094315dddbd7e3b83588",
+    "url": "/hello/static/js/2.2cb46bec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.2cb46bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4863a067f4b6df1778",
+    "url": "/hello/static/js/main.f4e1d5db.chunk.js"
+  },
+  {
+    "revision": "efc908b2a03c2a2c891f",
+    "url": "/hello/static/js/runtime-main.412ce529.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
